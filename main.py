f80962c6f853efc5717ceb8b43424549df7f576d
yaman-a <yaman.ashqar05@gmail.com>
2024-08-31 20:36:22 +0930

    model training works + display

--- main.py
+++ main.py
@@ -10,3 +10,46 @@ import matplotlib.pyplot as plt
 x_train = x_train / 255
 x_test = x_test / 255
 
+print(x_train.shape)
+
+# Build the model
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {test_accuracy}')
+
+
+
+# Testing out
+
+
+# # Pick a random test image
+# random_index = np.random.randint(0, len(x_test))
+# test_image = x_test[random_index]
+
+# # Save the test image to a file
+# plt.imshow(test_image, cmap='gray')
+# plt.savefig('test_image.png')
+
+# print('Test image saved as "test_image.png"')
+
+# # Expand dimensions since the model expects a batch (even if it's just one sample)
+# test_image = np.expand_dims(test_image, axis=0)
+
+# # Predict the digit using the model
+# predictions = model.predict(test_image)
+
+# # Get the index of the highest probability
+# predicted_digit = np.argmax(predictions[0])
+
+# print(f'Predicted digit: {predicted_digit}')
